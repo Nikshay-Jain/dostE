ae5a348e07e4bf99cac8cf30782cd9cf85ef0b19
Nikshay Jain <nikshay.p.jain@gmail.com>
2024-03-23 16:34:21 +0530

    Updated git.py

--- git.py
+++ git.py
@@ -3,26 +3,22 @@ import subprocess
 command = "git status"
 try:
     output = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
-    # print(output)
     file_lines = output.strip().split('\n')
     print(file_lines)
-
-    for i in range(6):
-        status = file_lines[i][1:2]
+    for i in range(len(file_lines)-7):
+        status = file_lines[i+6][1:2]
         print(status)
-        file_path = file_lines[i][12:]
-        print(file_path)
-    #     if status == 'd':
-    #         file_path = file_lines[i][13:]
-    #         print(file_path)
-    #         subprocess.run(f'git rm "{file_path}"')
-    #         subprocess.run(f'git commit -m "Removed {file_path}"')
+        if status == 'd':
+            file_path = file_lines[i+6][13:]
+            print(file_path)
+        #     subprocess.run(f'git rm "{file_path}"')
+        #     subprocess.run(f'git commit -m "Removed {file_path}"')
         
-    #     elif status == 'm':
-    #         file_path = file_lines[i][14:]
-    #         print(file_path)
-    #         subprocess.run(f'git add "{file_path}"')
-    #         subprocess.run(f'git commit -m "Updated {file_path}"')
+        elif status == 'm':
+            file_path = file_lines[i+6][13:]
+            print(file_path)
+            subprocess.run(f'git add "{file_path}"')
+            subprocess.run(f'git commit -m "Updated {file_path}"')
 
     #     else:
     #         file_path = file_lines[i][12:]
